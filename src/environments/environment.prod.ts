83410b0842381debe22fa352efa85b85af289fb3
John Nguyen <npjohn92@gmail.com>
2023-10-28 20:55:27 -0700

    edit for module6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  // api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://p6qvou0f06.execute-api.us-east-1.amazonaws.com'
 };
